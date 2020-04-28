4a64faa39e015b91df85da47d1769e002154d11d
pauladixon <paulapdixon@gmail.com>
2020-04-28 11:58:00 -0700

    added details page

--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render
+from .models import Cat
 
-class Cat:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-cats = [
-  Cat('Lolo', 'tabby', 'foul little demon', 3),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
 
 # Define the home view
 def home(request):
-    return render(request, 'home.html')
+  return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
 
 def cats_index(request):
-    return render(request, 'cats/index.html', { 'cats': cats})
+  cats = Cat.objects.all()
+  return render(request, 'cats/index.html', {'cats': cats})
+
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/detail.html', {'cat': cat})
+
