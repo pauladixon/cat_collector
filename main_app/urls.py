59ba699a77fd4b811a8e98dc844f237269452844
pauladixon <paulapdixon@gmail.com>
2020-04-29 09:58:44 -0700

    added class based views

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('cats/', views.cats_index, name='index'), 
-    path('kats/<int:cat_id>', views.cats_detail, name='cats_detail')
+    path('cats/', views.CatList.as_view(), name='cats_index'), 
+    path('cats/<int:pk>', views.CatDetail.as_view(), name='cats_detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Cat
 
 
@@ -9,11 +10,18 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-def cats_index(request):
-  cats = Cat.objects.all()
-  return render(request, 'cats/index.html', {'cats': cats})
+# def cats_index(request):
+#   cats = Cat.objects.all()
+#   return render(request, 'cats/index.html', {'cats': cats})
 
-def cats_detail(request, cat_id):
-  cat = Cat.objects.get(id=cat_id)
-  return render(request, 'cats/detail.html', {'cat': cat})
+class CatList(ListView):
+  model = Cat
+  def get_queryset(self):
+    return Cat.objects.all()
 
+# def cats_detail(request, cat_id):
+#   cat = Cat.objects.get(id=cat_id)
+#   return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatDetail(DetailView):
+  model = Cat
