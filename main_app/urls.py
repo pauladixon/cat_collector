42ae887943f67399c6b67630666faeefa544f324
pauladixon <paulapdixon@gmail.com>
2020-04-30 09:29:03 -0700

    finished adding class based views

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('cats/<int:pk>', views.CatDetail.as_view(), name='cats_detail'),
     path('cats/create', views.CatCreate.as_view(), name='cats_create'),
     path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cats_update'),
-    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cats_delete')
+    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cats_delete'),
 ]
